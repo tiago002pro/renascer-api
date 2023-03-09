1a8c80dca0358610f7748cd02c7d09f122d8bf8c
tiago002pro <tiagobarbosa02@outlook.com>
2023-03-09 13:40:35 -0300

    Iniciando projeto spring boot

---
+++ src/main/java/com/api/renascer/RenascerApplication.java
@@ -0,0 +1,13 @@
+package com.api.renascer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenascerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenascerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/renascer/RenascerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.renascer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenascerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
