3f17cc0194aebc2949c569b5f14427df121b7ae6
tiago002pro <tiagobarbosa02@outlook.com>
2024-02-25 23:10:15 -0300

    Query para a inserção da agenda

---
+++ target/classes/liquibase/v1/scripts/schedule.sql
@@ -0,0 +1,52 @@
+INSERT INTO public.schedule (title, start_date, end_date, address, description, image, registration, deadline)
+VALUES
+    (
+        'Conferência',
+        '2024-02-22 22:16:49.072',
+        '2024-02-22 22:16:49.072',
+        'R. Pioneiro José Jacinto Maia, 250 - Jardim Copacabana, Maringá - PR',
+        'Sem descrição',
+        'https://firebasestorage.googleapis.com/v0/b/renascer-app.appspot.com/o/CONF-1.jpg?alt=media&token=2235eb59-51f5-481a-bf41-8a2f8f5104f9',
+        false,
+        '2024-05-01 00:00:00.000'
+    ),
+    (
+        'Acampamento',
+        '2024-08-10 00:00:00.000',
+        '2024-08-15 00:00:00.000',
+        'R. Pioneiro José Jacinto Maia, 250 - Jardim Copacabana, Maringá - PR',
+        'Sem descrição',
+        'https://firebasestorage.googleapis.com/v0/b/renascer-app.appspot.com/o/CONF-5.jpg?alt=media&token=778d6bd0-3b51-4ea3-8d2f-252d202d5c35',
+        false,
+        '2024-05-01 00:00:00.000'
+    ),
+    (
+        'Culto Jovem',
+        '2024-05-12 19:00:00.000',
+        '2024-05-12 21:00:00.000',
+        'R. Pioneiro José Jacinto Maia, 250 - Jardim Copacabana, Maringá - PR',
+        'Sem descrição',
+        '',
+        false,
+        '2024-05-13 00:00:00.000'
+    ),
+    (
+        'Culto Jovem',
+        '2024-06-12 19:00:00.000',
+        '2024-06-12 21:00:00.000',
+        'R. Pioneiro José Jacinto Maia, 250 - Jardim Copacabana, Maringá - PR',
+        'Sem descrição',
+        '',
+        false,
+        '2024-06-13 00:00:00.000'
+    ),
+    (
+        'Culto Jovem',
+        '2024-07-12 19:00:00.000',
+        '2024-07-12 21:00:00.000',
+        'R. Pioneiro José Jacinto Maia, 250 - Jardim Copacabana, Maringá - PR',
+        'Sem descrição',
+        '',
+        false,
+        '2024-07-13 00:00:00.000'
+    );
