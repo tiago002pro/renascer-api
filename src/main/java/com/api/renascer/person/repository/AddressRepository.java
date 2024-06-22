8dcccb21f85568be51a0af2d4dbc633e0a1f18cb
tiago002pro <tiagobarbosa02@outlook.com>
2024-06-22 12:16:33 -0300

    Clean code

--- src/main/java/com/api/renascer/person/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.renascer.person.repository;
-
-import com.api.renascer.person.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
