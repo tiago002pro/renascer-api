8be16f8916cfdd47ecccd69bfd31075925f51ed6
tiago002pro <tiagobarbosa02@outlook.com>
2024-07-21 11:32:13 -0300

    Configurando as origens permitidas cors

--- src/main/java/com/api/renascer/infra/security/CorsConfig.java
+++ src/main/java/com/api/renascer/infra/security/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
            @Override
            public void addCorsMappings(CorsRegistry registry) {
                registry.addMapping("/**")
-                   .allowedOrigins("*")
+                   .allowedOrigins("http://localhost:4200, https://igrejarenascer.org.br")
                    .allowedMethods("*")
                    .allowedHeaders("*");
            }
